db2a07a16ab52ba0518387be0dd7499ed628d81d
萝卜丝 <luobosi@2dfire.com>
2018-11-15 08:10:12 +0800

    缓存相关知识点总结

--- src/main/java/org/apache/ibatis/cache/CacheKey.java
+++ src/main/java/org/apache/ibatis/cache/CacheKey.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.cache;
 
@@ -23,121 +23,135 @@ import org.apache.ibatis.reflection.ArrayUtil;
 
 /**
  * 缓存 key
+ * 在 Cache 中唯一确定一个缓存项需要使用缓存项的 key
+ * MyBatis 中因为涉及动态 SQL 等多方面因素，其缓存项的 key 不能仅仅通过一个 String 标示，
+ * 所以 MyBatis 提供了 CacheKey 类来表示缓存项的 key，在一个 CacheKey 对象中可以封装多个
+ * 影响缓存项的因素。
  *
  * @author Clinton Begin
  */
 public class CacheKey implements Cloneable, Serializable {
 
-  private static final long serialVersionUID = 1146682552656046210L;
+    private static final long serialVersionUID = 1146682552656046210L;
 
-  public static final CacheKey NULL_CACHE_KEY = new NullCacheKey();
+    public static final CacheKey NULL_CACHE_KEY = new NullCacheKey();
 
-  private static final int DEFAULT_MULTIPLYER = 37;
-  /**
-   * 默认的 hashcode
-   */
-  private static final int DEFAULT_HASHCODE = 17;
+    private static final int DEFAULT_MULTIPLYER = 37;
+    /**
+     * 默认的 hashcode
+     */
+    private static final int DEFAULT_HASHCODE = 17;
 
-  private final int multiplier;
-  /**
-   * 哈希值
-   */
-  private int hashcode;
-  /**
-   * 冗余校验
-   */
-  private long checksum;
-  /**
-   * 数量
-   */
-  private int count;
-  // 8/21/2017 - Sonarlint flags this as needing to be marked transient.  While true if content is not serializable, this is not always true and thus should not be marked transient.
-  private List<Object> updateList;
+    /**
+     * 参与计算的 hash 值
+     */
+    private final int multiplier;
+    /**
+     * 哈希值
+     */
+    private int hashcode;
+    /**
+     * 冗余校验值
+     */
+    private long checksum;
+    /**
+     * 数量
+     */
+    private int count;
+    // 8/21/2017 - Sonarlint flags this as needing to be marked transient.  While true if content is not serializable, this is not always true and thus should not be marked transient.
+    /**
+     * 以下四部分都会记录到该集合中
+     * 1. MappedStatement 的 id
+     * 2. 指定查询结果集的范围，也就是 RowBounds.offset 和 RowBounds.limit
+     * 3. 查询所使用的 SQL 语句，也就是 boundSql.getSql() 方法返回的 SQL 语句，其中可能包含 "?" 占位符
+     * 4. 用户传递给上述 SQL 语句的实际参数值
+     */
+    private List<Object> updateList;
 
-  public CacheKey() {
-    this.hashcode = DEFAULT_HASHCODE;
-    this.multiplier = DEFAULT_MULTIPLYER;
-    this.count = 0;
-    this.updateList = new ArrayList<Object>();
-  }
-
-  public CacheKey(Object[] objects) {
-    this();
-    updateAll(objects);
-  }
-
-  public int getUpdateCount() {
-    return updateList.size();
-  }
-
-  public void update(Object object) {
-    int baseHashCode = object == null ? 1 : ArrayUtil.hashCode(object); 
-
-    count++;
-    checksum += baseHashCode;
-    baseHashCode *= count;
-
-    hashcode = multiplier * hashcode + baseHashCode;
-
-    updateList.add(object);
-  }
-
-  public void updateAll(Object[] objects) {
-    for (Object o : objects) {
-      update(o);
-    }
-  }
-
-  @Override
-  public boolean equals(Object object) {
-    if (this == object) {
-      return true;
-    }
-    if (!(object instanceof CacheKey)) {
-      return false;
+    public CacheKey() {
+        this.hashcode = DEFAULT_HASHCODE;
+        this.multiplier = DEFAULT_MULTIPLYER;
+        this.count = 0;
+        this.updateList = new ArrayList<Object>();
     }
 
-    final CacheKey cacheKey = (CacheKey) object;
-
-    if (hashcode != cacheKey.hashcode) {
-      return false;
-    }
-    if (checksum != cacheKey.checksum) {
-      return false;
-    }
-    if (count != cacheKey.count) {
-      return false;
+    public CacheKey(Object[] objects) {
+        this();
+        updateAll(objects);
     }
 
-    for (int i = 0; i < updateList.size(); i++) {
-      Object thisObject = updateList.get(i);
-      Object thatObject = cacheKey.updateList.get(i);
-      if (!ArrayUtil.equals(thisObject, thatObject)) {
-        return false;
-      }
+    public int getUpdateCount() {
+        return updateList.size();
     }
-    return true;
-  }
 
-  @Override
-  public int hashCode() {
-    return hashcode;
-  }
+    public void update(Object object) {
+        int baseHashCode = object == null ? 1 : ArrayUtil.hashCode(object);
 
-  @Override
-  public String toString() {
-    StringBuilder returnValue = new StringBuilder().append(hashcode).append(':').append(checksum);
-    for (Object object : updateList) {
-      returnValue.append(':').append(ArrayUtil.toString(object));
+        count++;
+        checksum += baseHashCode;
+        baseHashCode *= count;
+
+        hashcode = multiplier * hashcode + baseHashCode;
+
+        updateList.add(object);
     }
-    return returnValue.toString();
-  }
 
-  @Override
-  public CacheKey clone() throws CloneNotSupportedException {
-    CacheKey clonedCacheKey = (CacheKey) super.clone();
-    clonedCacheKey.updateList = new ArrayList<Object>(updateList);
-    return clonedCacheKey;
-  }
+    public void updateAll(Object[] objects) {
+        for (Object o : objects) {
+            update(o);
+        }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof CacheKey)) {
+            return false;
+        }
+
+        final CacheKey cacheKey = (CacheKey) object;
+
+        if (hashcode != cacheKey.hashcode) {
+            return false;
+        }
+        if (checksum != cacheKey.checksum) {
+            return false;
+        }
+        if (count != cacheKey.count) {
+            return false;
+        }
+
+        for (int i = 0; i < updateList.size(); i++) {
+            Object thisObject = updateList.get(i);
+            Object thatObject = cacheKey.updateList.get(i);
+            if (!ArrayUtil.equals(thisObject, thatObject)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return hashcode;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder returnValue = new StringBuilder().append(hashcode).append(':').append(checksum);
+        for (Object object : updateList) {
+            returnValue.append(':').append(ArrayUtil.toString(object));
+        }
+        return returnValue.toString();
+    }
+
+    @Override
+    public CacheKey clone() throws CloneNotSupportedException {
+        CacheKey clonedCacheKey = (CacheKey) super.clone();
+        clonedCacheKey.updateList = new ArrayList<Object>(updateList);
+        return clonedCacheKey;
+    }
 
 }
